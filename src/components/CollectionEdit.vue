d10010c93dc26e48d3ae4b051ca9a18d5a5026a0
Damien DURO <damien@weweb.io>
2021-07-07 11:13:31 +0200

    Version 3.0.0

--- src/components/CollectionEdit.vue
+++ src/components/CollectionEdit.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="javascript-collection-edit">
         <wwEditorFormRow label="Javascript code">
-            <wwCodeEditor name="js-code" :value="content.js" @input="setProp('js', $event)" />
+            <wwCodeEditor name="js-code" :model-value="content.js" @update:modelValue="setProp('js', $event)" />
         </wwEditorFormRow>
     </div>
 </template>
@@ -9,21 +9,10 @@
 <script>
 export default {
     props: {
-        plugin: { type: Object, required: true },
         config: { type: Object, required: true },
     },
-    watch: {
-        isSetup: {
-            immediate: true,
-            handler(value) {
-                this.$emit('update-is-valid', value);
-            },
-        },
-    },
+    emits: ['update:config'],
     computed: {
-        isSetup() {
-            return !!this.content.js;
-        },
         content() {
             return {
                 js: 'return { test: "my code is always working" }',
@@ -31,13 +20,13 @@ export default {
             };
         },
     },
-    methods: {
-        setProp(key, value) {
-            this.$emit('update-config', { ...this.content, [key]: value });
-        },
-    },
     mounted() {
         this.setProp('js', this.content.js);
+    },
+    methods: {
+        setProp(key, value) {
+            this.$emit('update:config', { ...this.content, [key]: value });
+        },
     },
 };
 </script>
