6b43b9637b7a1b7049b4392f59b20ac8ce2b5190
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-14 22:22:45 +0100

    feat: add copilot configuration

--- ww-config.js
+++ ww-config.js
@@ -9,6 +9,19 @@ export default {
             getIsValid(config) {
                 return !!config.js;
             },
+            /* wwEditor:start */
+            copilot: {
+                description: 'Execute a custom JavaScript code to fetch and transform data. The code runs as an async function and must return the data to be used as a collection.',
+                returns: 'any',
+                schema: {
+                    js: {
+                        type: 'string',
+                        description: 'JavaScript code to execute. You can use async/await and the axios library is available globally.',
+                        bindable: false
+                    }
+                }
+            }
+            /* wwEditor:end */
         },
     },
 };
